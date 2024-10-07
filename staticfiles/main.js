2bed1dd80a7a9c02730c2eb98e82c9ba2293b11f
shinhosuck <shinhosuck1973@gmail.com>
2024-10-08 03:06:18 +0800

    added staticfiles

--- staticfiles/main.js
+++ staticfiles/main.js
@@ -1,6 +1,7 @@
+
+// Messages from backend: success, error, warning, info, and etc.
 const messages = document.querySelector('.messages')
 const message = document.querySelector('.message')
-
 
 function closeMessageBtn() {
     messages.classList.add('close-messages')
@@ -10,3 +11,27 @@ if (messages) {
     const messageWidth = message.clientWidth 
     message.style.width = `${messageWidth}px`
 }
+
+
+// Mobile navbar: toggle mobile navlinks
+const mobileNavLinksToggleBtnsContainer = document.querySelector('.mobile-navlinks-toggle-btns-container')
+const navLinksToggleBtns = [...mobileNavLinksToggleBtnsContainer.querySelectorAll('button')]
+const mobileNavLinks = document.querySelector('.mobile-navlinks')
+
+const openBtn = document.querySelector('.mobile-navlinks-open-btn')
+const closeBtn = document.querySelector('.mobile-navlinks-close-btn')
+
+navLinksToggleBtns.forEach((btn) => {
+    btn.addEventListener('click', (e)=> {
+        mobileNavLinks.classList.toggle('show-mobile-navlinks')
+
+        if (e.currentTarget === openBtn) {
+            e.currentTarget.classList.add('hide-mobile-navlinks-toggle-btn')
+            closeBtn.classList.remove('hide-mobile-navlinks-toggle-btn')
+        }
+        else if (e.currentTarget === closeBtn) {
+            e.currentTarget.classList.add('hide-mobile-navlinks-toggle-btn')
+            openBtn.classList.remove('hide-mobile-navlinks-toggle-btn')
+        }
+    })
+})
