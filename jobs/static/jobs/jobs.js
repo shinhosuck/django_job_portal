f9fe0bd4a93d3e3f9f35cbdf721938a73a16b35e
shinhosuck <shinhosuck1973@gmail.com>
2024-10-09 07:13:52 +0800

    update

--- jobs/static/jobs/jobs.js
+++ jobs/static/jobs/jobs.js
@@ -3,3 +3,26 @@ const contactForm = Array.from(document.querySelectorAll('.contact-form > p'))
 contactForm.forEach((p)=> {
     p.setAttribute('class', 'contact-form-row')
 })
+
+// Mobile navbar: toggle mobile navlinks
+const mobileNavLinksToggleBtnsContainer = document.querySelector('.landing-page-mobile-navlinks-toggle-btns-container')
+const navLinksToggleBtns = [...mobileNavLinksToggleBtnsContainer.querySelectorAll('button')]
+const mobileNavLinks = document.querySelector('.landing-page-mobile-navlinks')
+
+const openBtn = document.querySelector('.landing-page-mobile-navlinks-open-btn')
+const closeBtn = document.querySelector('.landing-page-mobile-navlinks-close-btn')
+
+navLinksToggleBtns.forEach((btn) => {
+    btn.addEventListener('click', (e)=> {
+        mobileNavLinks.classList.toggle('landing-page-show-mobile-navlinks')
+
+        if (e.currentTarget === openBtn) {
+            e.currentTarget.classList.add('landing-page-hide-mobile-navlinks-toggle-btn')
+            closeBtn.classList.remove('landing-page-hide-mobile-navlinks-toggle-btn')
+        }
+        else if (e.currentTarget === closeBtn) {
+            e.currentTarget.classList.add('landing-page-hide-mobile-navlinks-toggle-btn')
+            openBtn.classList.remove('landing-page-hide-mobile-navlinks-toggle-btn')
+        }
+    })
+})
--- staticfiles/main.js
+++ staticfiles/main.js
@@ -3,6 +3,7 @@
 const messages = document.querySelector('.messages')
 const message = document.querySelector('.message')
 
+
 function closeMessageBtn() {
     messages.classList.add('close-messages')
 }
@@ -13,25 +14,27 @@ if (messages) {
 }
 
 
-// Mobile navbar: toggle mobile navlinks
-const mobileNavLinksToggleBtnsContainer = document.querySelector('.mobile-navlinks-toggle-btns-container')
-const navLinksToggleBtns = [...mobileNavLinksToggleBtnsContainer.querySelectorAll('button')]
-const mobileNavLinks = document.querySelector('.mobile-navlinks')
+// Toggle mobile navlinks
+const mobileNavlinks = document.querySelector('.mobile-navlinks')
+const toggleBtnsContainer = document.querySelector('.mobile-navlinks-toggle-btns-container')
+const toggleBtns = toggleBtnsContainer && 
+                Array.from(toggleBtnsContainer.querySelectorAll('button'))
 
-const openBtn = document.querySelector('.mobile-navlinks-open-btn')
-const closeBtn = document.querySelector('.mobile-navlinks-close-btn')
+const mobileOpenBtn = document.querySelector('.mobile-navlinks-open-btn')
+const mobileCloseBtn = document.querySelector('.mobile-navlinks-close-btn')
 
-navLinksToggleBtns.forEach((btn) => {
-    btn.addEventListener('click', (e)=> {
-        mobileNavLinks.classList.toggle('show-mobile-navlinks')
+toggleBtns && toggleBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        mobileNavlinks.classList.toggle('show-mobile-navlinks')
 
-        if (e.currentTarget === openBtn) {
-            e.currentTarget.classList.add('hide-mobile-navlinks-toggle-btn')
-            closeBtn.classList.remove('hide-mobile-navlinks-toggle-btn')
+        if (btn === mobileOpenBtn) {
+            mobileOpenBtn.classList.add('hide-mobile-navlinks-toggle-btn')
+            mobileCloseBtn.classList.remove('hide-mobile-navlinks-toggle-btn')
         }
-        else if (e.currentTarget === closeBtn) {
-            e.currentTarget.classList.add('hide-mobile-navlinks-toggle-btn')
-            openBtn.classList.remove('hide-mobile-navlinks-toggle-btn')
+        else if(btn === mobileCloseBtn) {
+            mobileOpenBtn.classList.remove('hide-mobile-navlinks-toggle-btn')
+            mobileCloseBtn.classList.add('hide-mobile-navlinks-toggle-btn')
         }
     })
 })
+
